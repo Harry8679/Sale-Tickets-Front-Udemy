ed33bd6817dfbffb6e8b48e6da74fc65a0e1c40e
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-26 16:03:05 +0100

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../slices/authSlice";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+
+  return (
+    <nav className="bg-blue-600 p-4 text-white flex justify-between">
+      <Link to="/" className="text-lg font-bold">🎟️ EventBooking</Link>
+      <div>
+        {user ? (
+          <>
+            <Link to="/profile" className="mr-4">Profil</Link>
+            <button onClick={handleLogout} className="bg-red-500 px-3 py-1 rounded">Déconnexion</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="mr-4">Connexion</Link>
+            <Link to="/register" className="bg-green-500 px-3 py-1 rounded">Inscription</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
